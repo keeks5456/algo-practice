f7965ea35873046d3db707cd026445ff88f446dd
keeks5456 <Akikogreen08@gmail.com>
2020-11-04 20:38:24 -0800

    creating leetcode folders

---
+++ leetcode/easy/reverseInt.js
@@ -0,0 +1,72 @@
+// 7. Reverse Integer
+// Easy
+
+// 3908
+
+// 6063
+
+// Add to List
+
+// Share
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Note:
+// Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+ 
+
+// Example 1:
+
+// Input: x = 123
+// Output: 321
+// Example 2:
+
+// Input: x = -123
+// Output: -321
+// Example 3:
+
+// Input: x = 120
+// Output: 21
+// Example 4:
+
+// Input: x = 0
+// Output: 0
+ 
+
+// Constraints:
+
+// -231 <= x <= 231 - 1
+
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let numbers = ("" + x).split("").reverse().join("")
+    console.log(numbers)
+    return numbers
+};
+// this only works for positive numbers 
+
+// var newNum = []
+// const reverse = (x) =>{
+//     let numbers = ("" + x).split("")
+//     for(let i = 0; i < numbers.length; i++){
+//     let popped = numbers.pop()
+//     }
+//     newNum.push(popped)
+//     console.log(newNum)
+    
+//     return newNum.join("")
+//     }
+
+var reverse = function(x) {
+    
+  const reversedInt = parseInt(Math.abs(x).toString().split('').reverse().join(''));
+  
+  if (reversedInt > 2**31) return 0;
+  
+  return reversedInt * Math.sign(x);
+    
+};
