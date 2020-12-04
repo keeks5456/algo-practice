2519e55034152cf1d4de93544d76763828fab270
keeks5456 <Akikogreen08@gmail.com>
2020-12-03 19:15:06 -0800

    factorialize problem

--- freecodeCamp/basic-algo.js
+++ freecodeCamp/basic-algo.js
@@ -36,4 +36,48 @@ function reverseString(str) {
   }
 
 //The ** substr() method returns a portion of the string, starting at the specified index and extending for a given number of characters afterwards.
+
+const str = 'Mozilla';
+
+console.log(str.substr(1, 2));
+// expected output: "oz"
+
+console.log(str.substr(2));
+// expected output: "zilla"
+
+
+
+// Factorialize a Number
+
+// Return the factorial of the provided integer.
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+// Factorials are often represented with the shorthand notation n!
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+// Only integers greater than or equal to zero will be supplied to the function.
+
+// Recursive Solution
+function factorialize(num) {
+    if(num < 0){
+      return -1
+    } else if(num == 0){
+      return 1
+    } else {
+     return num * factorialize(num - 1)
+    }
+  }
   
+  factorialize(5);
+  
+//Using a For Loop
+  const factorialize = (num) =>{
+      for(var product = 1; num > 0, num --){
+          product *= num 
+      }
+      return product
+  }
+
+//   Since the return values for the function will always be greater than or equal to 1, product is initialized at one. For the case where the number is 0, the for loop condition will be false, but since product is initialized as 1, it will have the correct value when the return statement is executed.
+
+//   For all numbers passed to the function which are greater than one, the simple for loop will decrement num by one each iteration and recalculate product down to the value 1.
+  
+
