e0350145461ff2c41f1280a462c321472ecebf2d
keeks5456 <Akikogreen08@gmail.com>
2020-12-22 12:34:30 -0800

    inorder traversal

--- leetcode/leetCodeExploreNotes/inorder_traversal.js
+++ leetcode/leetCodeExploreNotes/inorder_traversal.js
@@ -1,6 +1,6 @@
 // Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
-
+// notes: root is a (binary) tree that optionally contains a left and/or right (binary) tree.
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -13,22 +13,46 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-// if root is empty return an empty array
-//  create an empty stack
-// assign a current variable to the root
-// push current variable to stack and set current = to current.left till null
-// if current === null && stack isnt empty then:
-    // pop tp item from stack
-    // print the popped item and set current to popped_item.right
-    // repeat step 3
-// if current === null && stack === empty then return result..?
+
 var inorderTraversal = function(root) {
-    if(root === null){
-        return []
+    var callStack = [];
+    var current = root;
+    var result = [];
+    while (true) {
+      while (!!current) { //while current isnt empty
+          callStack.push(current); //push the root int our callStack
+          current = current.left; //keepp moving left down the tree
+      }
+
+      if (callStack.length == 0) break; // have a break in the loop if the callStack becomes empty.
+
+      var lastCurrent = callStack.pop(); //pop off the remaining nodes into a new variable
+        result.push(lastCurrent.val); //push into result the values of lastCurrent FILO
+        current = lastCurrent.right; // keep moving right down the tree
     }
+      return result //return the results
+  }
 
-    let stack = []
-    let current = root 
 
-    stack.push(current.left)
-};
+//   Recursive solution 
+
+function inorderTraversal(root){
+    let result = [] //assign a result variable to return later
+
+    if(!!root.left){
+        //the root left side is not empty
+        result = result.concat(inorderTraversal(root.left)) //keep recursively concating the left nodes in our results
+        result = result.push(root.val)
+        //take our result and push the values into it
+    }
+    //same concept for right side of root
+
+    if(!!root.right){
+        result = result.concat(inorderTraversal(root.right))
+        result = result.push(root.val)
+    }
+    return result 
+}
+
+// another iterative solution
+
