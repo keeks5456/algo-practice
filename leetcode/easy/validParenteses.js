159d8a37fdf14018d1d5948fa6923a51eb98c068
keeks5456 <Akikogreen08@gmail.com>
2021-02-02 22:56:11 -0800

    added notes aand alternative solution

--- leetcode/easy/validParenteses.js
+++ leetcode/easy/validParenteses.js
@@ -65,3 +65,41 @@ var isValid = function(s) {
 // - If we encounter an opening bracket, we simply push it onto the stack. This means we will process it later, let us simply move onto the sub-expression ahead.
 // - If we encounter a closing bracket, then we check the element on top of the stack. If the element at the top of the stack is an opening bracket of the same type, then we pop it off the stack and continue processing. Else, this implies an invalid expression.
 // - In the end, if we are left with a stack still having elements, then this implies an invalid expression.
+
+//more notes
+
+white boarding
+
+// the key is use a stack
+// the only thing added to the stack are these [ { (
+// if you come across any f these open symbols, add to stack 
+// if not one of these, compare to whats at top of stack
+
+
+// [()]
+// start with first element [ <-- open? yes -- add to stack 
+// next element ( <-- open ? yes -- add to stack 
+// next ) <-- open ? no -- compare to an open parenthesis @ stack, does it close ? yes 
+// pop this off top of stack
+//  
+
+// solution with a for of loop
+
+var isValid = function(s){
+    let stack = new stack()
+
+    for(character of s){ //iterate over every character in the string
+        if(character === '(' || character === '{' || character === '['){ // push any of these open symbols to the stack
+         stack.push(character)
+        } else if(!stack.isEmpty() && character === ')' && stack.peek() === '('){ //as long as the stack is nt empty we can do these -->. peek at the top to see if this closing symbol has a valid opening symbol, also, check if character is closing symbol
+            stack.pop()
+        } else if(!stack.isEmpty() && character === '}' && stack.peek() === '{'){
+            stack.pop()
+        } else if(!stack.isEmpty() && character === ']' && stack.peek() === '['){
+            stack.pop()
+        } else { //have the case in which if these are nt valid we return false
+            return false
+        }
+    }
+    return stack.isEmpty() //check to see if stack is empty
+}
