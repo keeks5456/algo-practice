ea43bfea4379540b924aa47eea66fa25a1159e3f
keeks5456 <Akikogreen08@gmail.com>
2021-02-23 19:03:58 -0800

    hash tables complete

--- Udemy-folder/HashTable.js
+++ Udemy-folder/HashTable.js
@@ -71,3 +71,45 @@ console.log(myHT.buckets)
 //dean and dane are a collision
 
 
+//Get Method
+HashTable.prototype.get = function(key){
+    var index = this.hash(key)
+    if(!this.buckets[index]) return null
+    else {
+      var currentNode = this.buckets[index]
+      while(currentNode){
+        if(currentNode.key === key)return currentNode.value
+        currentNode = currentNode.next
+      }
+      return null
+    }
+  }
+  
+  HashTable.prototype.retrieveAll = function(){
+    var allNodes = []
+    for(let i = 0; i < this.numBuckets; i++){
+      let currentNode = this.buckets[i]
+      while(currentNode){
+        allNodes.push(currentNode)
+        currentNode = currentNode.next
+      }
+    }
+    return allNodes
+  }
+  
+  myHT.insert('Dean', 'karen@gmail.com', )
+  myHT.insert('Megan', 'megan@gmail.com', )
+  myHT.insert('Megan', 'meganGriffin@gmail.com')
+  myHT.insert('Dane', 'kathy@yahoo.com')
+  myHT.insert('Dane', 'dane1010@gmail.com')
+  myHT.insert('Joe', 'JoeJames@gmail.com')
+  myHT.insert('Samantha', 'sammy@twitter.com')
+  
+  
+  myHT.retrieveAll()
+  // console.log(myHT.buckets)
+  //dean and dane are a collision
+  
+  
+  
+    
